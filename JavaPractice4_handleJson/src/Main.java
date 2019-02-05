dff96c5a70152e86ebcc2d880ae3d97b763d65ae
aitorgit <tugasmec@gmail.com>
2019-02-05 13:31:46 +0100

    added jsonData

--- JavaPractice4_handleJson/src/Main.java
+++ JavaPractice4_handleJson/src/Main.java
@@ -1,12 +1,14 @@
+import jsonpckge.ReadJson;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String
+		ReadJson.getFileAndParseToJsonObj(filePath, objectName);
 
 	}
-	ClassLoader classLoader = this.getClass().getClassLoader();
-	File file = new File();
+
 	
 	// falta implementar metodos de consumo total, CLASSE people, classe para gestionar listas
 
--- JavaPractice4_handleJson/src/jsonpckge/ReadJson.java
+++ JavaPractice4_handleJson/src/jsonpckge/ReadJson.java
@@ -1,21 +1,37 @@
 package jsonpckge;
 
+import java.io.File;
+import java.io.FileReader;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class ReadJson {
 	
 	
-	public void getFile(String filePath) {
-		ClassLoader classLoader = this.getClass().getClassLoader();
+	public static JSONObject getFileAndParseToJsonObj(String filePath, String objectName) {
+		
+		ClassLoader classLoader = new ReadJson().getClass().getClassLoader();
 
-File file = new File(classLoader.getResource(fileName).getFile());
-
-JSONParser parser = new JSONParser();
-
-try{
-	FileReader reader = new FileReader(file.getAbsolutePath());
-	Object obj = parser.parse(reader);
-	JSONObject jsonObj = (JSONObject)obj;
-	JSONObject 
-}
+		File file = new File(classLoader.getResource(filePath).getFile());
+		
+		JSONParser parser = new JSONParser();
+		
+		try{
+			FileReader reader = new FileReader(file.getAbsolutePath());
+			Object obj = parser.parse(reader);
+			JSONObject jsonObj = (JSONObject)obj;
+			JSONObject jsonObjElement = (JSONObject)jsonObj.get(objectName);
+			return jsonObjElement;
+			
+		} catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
+	
+	
+	
 
 }
